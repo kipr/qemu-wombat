09b1438758707fabf9ae96e167610030207d5133
tcorbly <45633570+tcorbly@users.noreply.github.com>
2022-08-31 12:53:15 -0600

    Fix Wombat_Start to match readme

--- Wombat_Start.sh
+++ Wombat_Start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-sudo qemu-system-arm -kernel kernel-qemu \
--cpu arm1176 -m 256 \
--M versatilepb -dtb versatile-pb-buster.dtb \
--no-reboot \
--serial stdio \
--append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
--hda Wombat-Bullseye.qcow2 \
--net nic -net user \
--net tap,ifname=vnet0,script=no,downscript=no
+sudo qemu-system-arm \
+  -M versatilepb \
+  -cpu arm1176 \
+  -m 256 \
+  -hda Wombat-Bullseye.img \
+  -net nic \
+  -net user,hostfwd=tcp::5022-:22 \
+  -dtb versatile-pb-buster.dtb \
+  -kernel kernel-qemu-4.19.50-buster \
+  -append 'root=/dev/sda2 panic=1' \
+  -no-reboot
